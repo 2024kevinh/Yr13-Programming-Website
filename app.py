d1a09888a61d71cad77e6458ec65d2e696a6cfbf
2024kevinh <2024kevinh@gmail.com>
2026-03-02 14:23:27 +1300

    Corrected name of files and added links to each page from the side bar

--- app.py
+++ app.py
@@ -25,9 +25,58 @@ def home():
             "rating": 4.9,
             "likes": 350,
             "image_url": "https://picsum.photos/400/700"
+        },
+        {
+            "title": "Cyberpunk Cityscape",
+            "creator": "Hacker",
+            "rating": 3.8,
+            "likes": 200,
+            "image_url": "https://picsum.photos/400/800"
+        },
+        {
+            "title": "photorealistic",
+            "creator": "ProPrompter",
+            "rating": 4.8,
+            "likes": 4050,
+            "image_url": "https://picsum.photos/400/900"
+        },
+        {
+            "title": "Quality",
+            "creator": "TopPrompter",
+            "rating": 3.0,
+            "likes": 30,
+            "image_url": "https://picsum.photos/400/300"
         }
     ]
     return render_template("index.html", prompts=prompts)
+
+@app.route("/trending")
+def trending():
+    return render_template("trending.html")
+
+@app.route("/foryou")
+def for_you():
+    return render_template("foryou.html")
+
+@app.route("/myprompts")
+def my_prompts():
+    return render_template("myprompts.html")
+
+@app.route("/subscriptions")
+def subscriptions():
+    return render_template("subscriptions.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
